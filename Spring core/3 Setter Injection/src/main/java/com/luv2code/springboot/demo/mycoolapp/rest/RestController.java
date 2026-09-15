a66eceffe5a9d14e4c8ca1d92de292770045d18c
Sherif-toiema22 <sherif.toiema22@gmail.com>
2026-09-15 12:53:32 +0300

    Setter Injection

--- Spring core/3 Setter Injection/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/RestController.java
+++ Spring core/3 Setter Injection/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/RestController.java
@@ -3,17 +3,18 @@ package com.luv2code.springboot.demo.mycoolapp.rest;
 import com.luv2code.springboot.demo.mycoolapp.service.paymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class FunRestController {
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
 
     private paymentService service;
 
     @Autowired
-    FunRestController (paymentService paymentService){
+    public void setRestController(paymentService paymentService){
         this.service = paymentService;
     }
+
+
     @GetMapping("/pay")
     public String pay(){
         return service.pay();
